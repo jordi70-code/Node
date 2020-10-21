817d66ad2c825e2a2aac3bfad9dd05e5bdc8d682
jordi70-code <57100732+jordi70-code@users.noreply.github.com>
2020-10-21 17:06:33 +0200

    post array + get ranking

--- 2damvi_server_1/src/index.js
+++ 2damvi_server_1/src/index.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.use(bodyParser.json());
 
+
+
 //Donde se guarda la informacion del jugador
 let jugador = {
     nombre:'',
@@ -11,6 +13,17 @@ let jugador = {
     score:''
 
 };
+
+
+let jugadors = [{
+        posicio: "0",
+        alies: "dummy",
+        nombre: "test",
+        apellidos: "test",
+        score: "0"
+}];
+
+
 
 //Respuesta de error
 let respuesta = {
@@ -20,15 +33,17 @@ let respuesta = {
 
 };
 
+
    //Jugador para demostrar si alguien ya esta creado
   let jugadorPepe = {
     nombre:'Pepe',
     apellidos:'Moash',
     score:'2'
-};
-
+    };
+    
+    
     //El post le pido los tres apartados de jugador
-   app.post('/jugador', function (req, res) {
+   app.post('/jugadors', function (req, res) {
        
     if(!req.body.nombre || !req.body.apellidos || !req.body.score) { //Si uno de los tres es nulo da error
         respuesta = {
@@ -47,20 +62,51 @@ let respuesta = {
             };
         } 
         else {  //No da errores y muestra el resultado
-            jugador = {
-                nombre: req.body.nombre,
-                apellidos: req.body.apellidos
-                };
+           
+            
+                jugadors[jugadors.length] = {
+                    posicio: jugadors.length,
+                    nombre: req.body.nombre,
+                    apellidos: req.body.apellidos,
+                    alies: req.body.alies,
+                    score: req.body.score
+                    };
+                
+
+            
+            
+            
             respuesta = {
                 error: false,
                 codigo: 200,
                 mensaje: 'jugador creado',
-                respuesta: jugador
+                respuesta: jugadors[jugadors.length - 1]
                 };
             }
     }    
     res.send(respuesta);
    });
+
+
+
+    app.get('/ranking', function (req, res) {
+    
+        jugadors.sort((a,b)  => (a.score < b.score ? 1 : -1));
+        for(i = 0; i < jugadors.length; i++){
+            jugadors[i].posicio = i+1;
+
+        }
+        var last = jugadors[jugadors.length - 1];
+        if (last.score == 0){
+
+            jugadors.splice(-1,1);
+
+        }
+        
+   
+
+    res.send(jugadors);
+    });
 
 app.listen(3000, () => {
 console.log("El servidor esta iniciado");
