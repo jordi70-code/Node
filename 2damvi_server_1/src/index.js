8074dea9ab7c658af59a62ab5660a6318511eff4
jordi70-code <57100732+jordi70-code@users.noreply.github.com>
2020-10-28 15:30:37 +0100

    cambios index put

--- 2damvi_server_1/src/index.js
+++ 2damvi_server_1/src/index.js
@@ -33,18 +33,10 @@ let respuesta = {
     mensaje:''
 
 };
-
-
-   //Jugador para demostrar si alguien ya esta creado
-  let jugadorPepe = {
-    nombre:'Pepe',
-    apellidos:'Moash',
-    score:'2'
-    };
-    
+  
     
     //El post le pido los tres apartados de jugador
-   app.post('/jugadors/:alias', function (req, res) {
+app.post('/jugadors/:alias', function (req, res) {
        
     if(!req.body.nombre || !req.body.apellidos || !req.body.score || !req.params.alias) { //Si uno de los tres es nulo da error
         respuesta = {
@@ -54,15 +46,16 @@ let respuesta = {
         };
     } 
     else {
-        if(req.body.nombre == jugadorPepe.nombre && req.body.apellidos == jugadorPepe.apellidos) { //Si los dos, el nombre y el apellido estan repetidos da error
-          
-            respuesta = {
-            error: true,
-            codigo: 503,
-            mensaje: 'El jugador ya fue creado previamente, deja de registrarte ' + jugadorPepe.nombre + " " + jugadorPepe.apellidos
-            };
-        } 
-        else {  //No da errores y muestra el resultado
+       for (i = 0; i < jugadors.length; i++){
+            if (jugadors[i].alies == req.params.alias){
+                respuesta = {
+                    error: true,
+                    codigo: 503,
+                    mensaje: 'Personaje ya creado'
+                };
+            }
+        }
+        if (!respuesta.error) {  //No da errores y muestra el resultado
            
             
                 jugadors[jugadors.length] = {
@@ -102,13 +95,14 @@ let respuesta = {
 
 
 
-app.put('/jugadors/:alies', function(req, res){  
-if(!req.body.nombre || !req.body.apellidos || !req.body.score || !req.params.alias) { //Si uno de los tres es nulo da error   
+app.put('/jugadors/:alias', function(req, res){  
+if(!req.body.nombre || !req.body.apellidos || !req.body.score || !req.params.alias || req.body.score <= 0) { //Si uno de los tres es nulo da error   
     respuesta = {
         error: false,
         codigo: 502,
         mensaje: 'Faltan campos en el put',
     }; 
+    
 }
 else{
     x = 0;
@@ -127,6 +121,7 @@ else{
             codigo: 504,
             mensaje: 'jugador no existe',
             };
+            
     }
     else {
         jugadors [jugadornum] = {
@@ -134,22 +129,37 @@ else{
             apellidos: req.body.apellidos,
             alies: req.params.alias,
             score: req.body.score
-            };
-        }
-    }
-
-    jugadors.sort((a,b)  => (a.score < b.score ? 1 : -1));
+        };
+        
+        jugadors.sort((a,b)  => (a.score < b.score ? 1 : -1));
         for(i = 0; i < jugadors.length; i++){
             jugadors[i].posicio = i+1;
+        }
 
         }
+    
+        respuesta = {
+            error: false,
+            codigo: 200,
+            mensaje: 'jugador creado',
+            respuesta: jugadors[jugadornum]
+            };
+
+    }
+    res.send(respuesta);
+    
 });
 
-    app.get('/ranking', function (req, res) {
-    res.send(jugadors);
-    });
+app.get('/ranking', function (req, res) {
+    respuesta = {
+        nombrejugadors: jugadors.length,
+        jugadores: jugadors 
 
-    app.get('/ranking/:alias', function (req, res) {
+    };
+    res.send(respuesta);
+});
+
+app.get('/ranking/:alias', function (req, res) {
       
         for(i = 0; i < jugadors.length; i++){
             
